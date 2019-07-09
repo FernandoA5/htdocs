bf1e9b5518a6cdac772132b2fd37fe7512e31c9a
FernandoA5 <fernandoalcalacasas@hotmail.com>
2019-07-09 16:41:11 -0500

    Intefaz
    Interfaz Registro Correcto

--- vistas/RegistroCorrecto.php
+++ vistas/RegistroCorrecto.php
@@ -11,7 +11,33 @@ include_once "Plantillas/bar.php";
 
 if($_GET["nombre"] && !empty($_GET["nombre"]))
 {
-  echo $_GET["nombre"];
+  //echo $_GET["nombre"];
+  ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+
+      </div>
+      <div class="col-md-4">
+        <div class="panel panel-primary">
+          <div class="panel-heading" align=center>
+            <h3 class="panel-title">Registro Correcto</h3>
+          </div>
+          <div class="panel-body">
+            <div class="text-center">
+              Felicidades <?php echo $_GET["nombre"]; ?> te has registrado correctamente<br><br>
+              <a href="<?php echo LOGIN; ?>">Iniciar Sesión</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+
+      </div>
+    </div>
+  </div>
+
+  <?php
 }
 else {
   redireccion::redirigir(SERVIDOR);
